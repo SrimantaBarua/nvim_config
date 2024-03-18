63a413cefe6343d071dea80d092d98210adfb170
Srimanta Barua <srimanta.barua1@gmail.com>
2024-03-17 18:12:23 -0700

    When opening nvim from terminal, flatten into current instance

--- lua/barua/plugins/terminal.lua
+++ lua/barua/plugins/terminal.lua
@@ -6,5 +6,16 @@ return {
         opts = {
             open_mapping = "<C-\\>t"
         }
-    }
+    },
+
+    -- When opening a file from a terminal buffer, open it in the current Neovim instance.
+    {
+        "willothy/flatten.nvim",
+        opts = {
+            window = { open = "alternate" }  -- Open in the previous window, not in the terminal's
+        },
+        -- Ensure that it runs first to minimize delay when opening file from terminal
+        lazy = false,
+        priority = 1001,
+    },
 }
