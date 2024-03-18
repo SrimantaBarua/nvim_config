e604ca91ed32254667c9859f73ede233671b5b01
Srimanta Barua <srimanta.barua1@gmail.com>
2024-03-17 18:01:36 -0700

    Add toggleterm for neovim terminal

---
+++ lua/barua/plugins/terminal.lua
@@ -0,0 +1,10 @@
+return {
+    -- Toggle multiple terminals during an editing session
+    {
+        "akinsho/toggleterm.nvim",
+        version = "*",
+        opts = {
+            open_mapping = "<C-\\>t"
+        }
+    }
+}
